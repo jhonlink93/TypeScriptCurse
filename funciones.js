cc277fd97986bf127f8bcd5eb3f750e55720c531
DreamCode <dreamcode@MacBook-Pro-de-DreamCode.local>
2021-09-10 08:29:40 -0500

    Hello Word

---
+++ funciones.js
@@ -0,0 +1,6 @@
+"use strict";
+var saludo = "hola ts primera page";
+function saludar() {
+    console.log("hola mundo", saludo);
+}
+saludar();
---
+++ funciones.ts
@@ -0,0 +1,6 @@
+let saludo = "hola ts primera page"
+
+function saludar(){
+    console.log("hola mundo",saludo);
+}
+saludar();
